2c44bbb3ab0a3912d975e95afbe99c4d053842ef
Marek <rkx1901@gmail.com>
2015-02-19 13:49:19 -0600

    Add user model

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,7 @@
 var todo = (function(){
   var initModule = function( $container ){
       todo.shell.initModule( $container ); 
+      todo.user.initModule();
   };
    return {initModule: initModule };
 }());

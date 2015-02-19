67dca5d3ede7d7848a6d427378925e6aef591574
Marek <rkx1901@gmail.com>
2015-02-19 17:37:38 -0600

    Add login

--- js/todo.acct.js
+++ js/todo.acct.js
@@ -2,7 +2,8 @@ todo.acct = (function(){
   
   var configMap = {
     form_html: function(form_type){
-      var new_form =  
+      var link,
+      new_form =  
      '<div class="row">'
       +'<div class="large-6 large-centered columns">'
       + '<form class="'
@@ -15,19 +16,50 @@ todo.acct = (function(){
          + '" id="'
          + form_type 
          + '" class="button small">'
-       + '</form>'
-      + '</div>'
-     + '</div>'
-       if(form_type === "login"){
-        new_form + '<a href="#" id="signup-form">Signup</a>'}
-        return new_form 
-      }
+       + '</form>';
+
+     if(form_type === "login"){
+       link = '<a href="#" id="signup-link">Register</a>';
+     }else{ 
+       link = '<a href="#" id="sigin-link">SignIn</a>';
+     }
+     new_form += link + '</div></div>'
+      return new_form 
+    }
   },
-   configModule, initModule ;
+   jqueryMap, auth_user, initModule ;
+
+  jqueryMap = { $form_container: $("#todo-form") }
+  auth_user = function(form_data, route){
+    
+    var email = form_data[0].value;
+    var password = form_data[1].value; 
+    var route = "http://recruiting-api.nextcapital.com/users/";
+    console.log("email ");
+    $.ajax({
+      url: route,
+      type: "POST",
+      data: {email: email, password: password}
+    })
+      .done(function(result){
+        localStorage.setItem("api_token", result.api_token);
+        localStorage.setItem("id", result.id);
+        console.log (todo.user.stateMap.user);
+      })
+      .fail(function(xhr,status, error){
+        console.log(error);
+        alert(error); 
+      })
   
+  }
   initModule = function(){
-    $("#todo-form").html(configMap.form_html("login"));
-  
+    if(localStorage.api_token === undefined ){
+      $("#todo-form").html(configMap.form_html("login"));
+    }  
+    $("form").on("submit", function(event){
+      event.preventDefault();
+      auth_user($(this).serializeArray());
+    })    
   }
 
     

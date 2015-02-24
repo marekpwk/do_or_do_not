5ac8f0f13aea0c51718512b9dd4ed1f298b4ba4a
Marek <rkx1901@gmail.com>
2015-02-24 11:54:52 -0600

    Changing css selectors

--- js/todo.shell.js
+++ js/todo.shell.js
@@ -10,7 +10,6 @@ todo.shell = (function(){
         + '<li class="name">'
           + '<h1><a href="#">DO OR DO NOT</a></h1>'
         + '</li>'
-         + '<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->'
         + '<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>'
       + '</ul>'
       + '<section class="top-bar-section">'
@@ -23,10 +22,9 @@ todo.shell = (function(){
       + '</nav>'
       + '<div class="row">'
         + '<div class="small-12 columns todo-main-content">'
-          + '<div id="todo-form"></div>'
-          + '<div id="todo-list">'
-            + '<div id="todo-add-item"></div>'
-          + '</div>'
+          + '<div id="todo-welcome-form"></div>'
+          + '<div id="todo-add-form"></div>'
+          + '<div id="todo-list" class="row"></div>'
       + '</div>'
    },
     stateMap = { $container : null },
